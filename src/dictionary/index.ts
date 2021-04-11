bcf1362213a0f9305c6ab372f21bb339da089498
Glauco Pater <>
2021-04-11 16:16:31 +0200

    Small update of the style

--- src/dictionary/index.ts
+++ src/dictionary/index.ts
@@ -1,8 +1,8 @@
 export const dictionary = {
     "appTitle": "Vibrant Form Demo",
     "submit": "Submit",
-    "isRequired": "field is required",
-    "shouldMatchPattern": "should match pattern",
-    "shouldBeGreater": "value should be greater",
+    "isRequired": "This value is required",
+    "shouldMatchPattern": "This value should match pattern",
+    "shouldBeGreater": "This value should be greater",
     "sendingData": "Sending data..."
 }
